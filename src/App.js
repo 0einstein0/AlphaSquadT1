c417d85fbf7336a8be84969fd2a6f99b897f92b2
0einstein0 <fzulfiqar.bscs18seecs@seecs.edu.pk>
2021-07-12 15:54:02 +0500

    -nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 
 import './App.css';
+import { BrowserRouter,Link, Route, Switch } from 'react-router-dom';
 import Homepage from  './pages/Homepage';
 import CaseStudies from './pages/CaseStudies';
 import CaseStudiesInner from './pages/CaseStudiesInner';
 import Pricing from './pages/Pricing';
+import NavigBar from './components/NavigBar';
 
 function App() {
-  return ( <CaseStudiesInner/>);
+  return ( 
+    <>
+
+    <BrowserRouter>
+         <Switch>
+    <Route exact path="/" component={Homepage}>
+    </Route>
+    <Route exact path="/Pricing" component={Pricing}>
+    </Route>
+    <Route exact path="/CaseStudies"  component={CaseStudies}  >
+    </Route>
+    <Route exact path="/CaseStudiesInner" component={CaseStudiesInner} >
+
+    </Route>
+  </Switch></BrowserRouter>
+  </>
+  );
 }
 
 export default App;

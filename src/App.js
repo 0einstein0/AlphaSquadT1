845cd0f0a6209d89eac05ed76777371b651c8462
0einstein0 <fzulfiqar.bscs18seecs@seecs.edu.pk>
2021-07-13 13:30:00 +0500

    -update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
-
-import './App.css';
-import { BrowserRouter,Link, Route, Switch } from 'react-router-dom';
-import Homepage from  './pages/Homepage';
-import CaseStudies from './pages/CaseStudies';
-import CaseStudiesInner from './pages/CaseStudiesInner';
-import Pricing from './pages/Pricing';
-import NavigBar from './components/NavigBar';
+import "./App.css";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import CaseStudies from "./pages/CaseStudies";
+import CaseStudiesInner from "./pages/CaseStudiesInner";
+import Pricing from "./pages/Pricing";
+import NavigBar from "./components/NavigBar";
 
 function App() {
-  return ( 
-    <>
-
+  return (
     <BrowserRouter>
-         <Switch>
-    <Route exact path="/" component={Homepage}>
-    </Route>
-    <Route exact path="/Pricing" component={Pricing}>
-    </Route>
-    <Route exact path="/CaseStudies"  component={CaseStudies}  >
-    </Route>
-    <Route exact path="/CaseStudiesInner" component={CaseStudiesInner} >
+      <nav>
+        <ul class="grid grid-flow-col gap-8">
+          <li>
+            <Link to="/">Overview</Link>
+          </li>
+          <li>
+            <Link to="/Pricing">Pricing</Link>
+          </li>
+          <li>
+            <Link to="/CaseStudies">Case Studies</Link>
+          </li>
+          <li>
+            <Link to="/CaseStudiesInner">FAQ</Link>
+          </li>
+        </ul>
+      </nav>
 
-    </Route>
-  </Switch></BrowserRouter>
-  </>
+      <Switch>
+        <Route exact path="/" component={Homepage}></Route>
+        <Route exact path="/Pricing" component={Pricing}></Route>
+        <Route exact path="/CaseStudies" component={CaseStudies}></Route>
+        <Route
+          exact
+          path="/CaseStudiesInner"
+          component={CaseStudiesInner}
+        ></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavigBar.js
+++ src/components/NavigBar.js
@@ -1,26 +1,37 @@
-import React, {Component} from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import Homepage from  '../pages/Homepage';
-import Pricing from '../pages/Pricing';
-import CaseStudies from '../pages/CaseStudies';
-import CaseStudiesInner from '../pages/CaseStudiesInner';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import Homepage from "../pages/Homepage";
+import Pricing from "../pages/Pricing";
+import CaseStudies from "../pages/CaseStudies";
+import CaseStudiesInner from "../pages/CaseStudiesInner";
 
-function NavigBar({...otherProps}) {
-    return <div {...otherProps} class=" px-16 py-7 self-center grid grid-flow-col auto-cols-max gap-4">
-          <BrowserRouter>
-          <nav >
-        <ul class="grid grid-flow-col gap-8">
-          <li><Link to="/Homepage">Overview</Link></li>
-          <li><Link to="/Pricing">Pricing</Link></li>
-          <li><Link to="/CaseStudies">Case Studies</Link></li>
-          <li><Link to="/CaseStudiesInner">FAQ</Link></li>   
-        </ul>
-      </nav>
- 
-</BrowserRouter>
-
-      </div>;
-  }
-  
+function NavigBar({ ...otherProps }) {
+  return (
+    <div
+      {...otherProps}
+      class=" px-16 py-7 self-center grid grid-flow-col auto-cols-max gap-4"
+    >
+      <BrowserRouter>
+        <nav>
+          <ul class="grid grid-flow-col gap-8">
+            <li>
+              <Link to="/">Overview</Link>
+            </li>
+            <li>
+              <Link to="/Pricing">Pricing</Link>
+            </li>
+            <li>
+              <Link to="/CaseStudies">Case Studies</Link>
+            </li>
+            <li>
+              <Link to="/CaseStudiesInner">FAQ</Link>
+            </li>
+          </ul>
+        </nav>
+        
+      </BrowserRouter>
+    </div>
+  );
+}
 
 export default NavigBar;
